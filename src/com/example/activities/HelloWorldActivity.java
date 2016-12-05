a68b451e326fe67bbb78907f8021723e0f53118f
盛聪 <@csc.lan>
2016-12-05 22:12:25 +0800

    1、增加activitywelcome.xml,用来显示登录成功后的欢迎页面
    2、改LoginActivity。java,为LOGIN和REGISTER按钮添加监听接口
    
    3、修改AndroidManifest.xml,注册activity_login
    
    4、存在问题:LoginActivity.java存在监听接口问题,尚未debug

--- src/com/example/activities/HelloWorldActivity.java
+++ src/com/example/activities/HelloWorldActivity.java
@@ -1,7 +1,20 @@
 package com.example.activities;
 
+import com.example.login.R;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View;
 
 public class HelloWorldActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+	}
 
 }
--- src/com/example/activities/LoginActivity.java
+++ src/com/example/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.activities;
 
+import com.example.fragments.SimpleTextInputCellFragment;
 import com.example.login.R;
 
 import android.app.Activity;
@@ -7,29 +8,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+	SimpleTextInputCellFragment fragInputCellAccount = (SimpleTextInputCellFragment) getFragmentManager()
+			.findFragmentById(R.id.input_username);
+	SimpleTextInputCellFragment fragInputCellPassword = (SimpleTextInputCellFragment) getFragmentManager()
+			.findFragmentById(R.id.input_password);
+	Button btn_login = (Button) findViewById(R.id.btn_login);
+	Button btn_register = (Button) findViewById(R.id.btn_register);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_activity);
-	
-		Button btn_login = (Button)findViewById(R.id.btn_login);
-		
-		
+
+		// 为LOGIN 按钮添加监听接口,按下跳转到欢迎界面(Hello World)
 		btn_login.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-			Toast toast=Toast.makeText(LoginActivity.this, "hello", Toast.LENGTH_SHORT);
-			toast.show();
-				
+
+				// setContentView(R.layout.activity_welcome);
+
+				Toast toast = Toast.makeText(LoginActivity.this, "hello", Toast.LENGTH_SHORT);
+				toast.show();
+
 			}
 		});
+
+		// 为REGISTER按钮添加监听接口,跳转到注册页面(Register)
+		btn_register.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				setContentView(R.layout.activity_register);
+
+			}
+		});
+
 	}
 
+	@Override
+	protected void onResume() {
+		fragInputCellAccount.setLabelText("帐户名");
+		fragInputCellAccount.setHintText("请输入账户名");
+		fragInputCellPassword.setLabelText("密码");
+		fragInputCellPassword.setHintText("请输入密码");
+		fragInputCellPassword.setIsPassword(true);
+	}
 
 }

817a25973fdc7ddd1294ebd2753daeac680c945f
Clevercsc <1046839730@qq.com>
2016-12-10 17:29:28 +0800

    增加HttpServer类.用于为所有的http请求创建一个OkHttpClient类,同时实现cookies

---
+++ src/model/HttpServer.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+
+import okhttp3.JavaNetCookieJar;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpServer {
+	//生成静态的client
+	public static  OkHttpClient client = new OkHttpClient();
+	//设置cookie
+	static{
+		CookieManager cookieManager = new CookieManager();
+		cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+		
+		client = new OkHttpClient.Builder()
+				.cookieJar(new JavaNetCookieJar(cookieManager))
+				.build();
+		
+	}
+	
+	//创建让其他类调用客户端的方法
+	public static OkHttpClient getSharedClient(){
+		return client;
+	}
+	
+	//创建调用服务器的api方法，例如 api=hello
+	public static Request.Builder requestBuilderWithApi(String api){
+		return new Request.Builder()
+				.url("http://172.27.0.10:8080/membercenter/api/"+api);
+	}
+	
+
+}
